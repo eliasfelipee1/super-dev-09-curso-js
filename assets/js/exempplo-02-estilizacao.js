dec856a47d8840c96a6d4e46ddb21e3d2987bd10
Elias Morais <12142@proway.treina>
2026-05-13 22:07:34 -0300

    estilizacao

---
+++ assets/js/exempplo-02-estilizacao.js
@@ -0,0 +1,79 @@
+//cria uma funcao para alternar a visibilidade da caixa
+function alternarVisibilidadeBotao(){
+    // busca no html o elemento que possui o id "caixa-01"
+    const divCaixa01 = document.getElementById("caixa-01");
+    // busca no html o botao que possui o id "botao-alternar-visibilidade"
+    const botaoAlternarVisibilidade = document.getElementById("botao-alternar-Visibilidade");
+
+    // verifica se a caixa esta sem valor de display ou escondida
+    if(divCaixa01.style.display === "" || divCaixa01.style.display === "none"){
+        // mostra a caixa na tela
+        divCaixa01.style.display = "block"; 
+        // altera o texto do botao para indicar que a caixa pode ser ocultada
+        botaoAlternarVisibilidade.innerText = "ocultar caixa";
+
+    // caso a caixa ja esteja visivel
+    } else {
+        //esconde a caixa da tela
+        divCaixa01.style.display = "none";
+        // altera o texto do botao para indicar que a caixa pode ser apresentada novamente
+        botaoAlternarVisibilidade.innerText = "apresentar caixa";
+
+    }
+
+}
+
+
+function definirCorVermelho(){
+    const divCaixa01 = decument.getElementById("caixa-01");
+    divCaixa01.classlist.remove("caixa-azul");
+    divCaixa01.classlist.add("caixa-vermelha");
+
+
+}
+
+function redefinirCor(){
+    const divCaixa01 = document.getElementById("caixa-01");
+    // apagar todas as classes atribuidas a div
+    divCaixa01.classList = [];
+
+}
+
+function definirCorAzul(){
+    const divCaixa01 = document.getElementById("caixa-01");
+    // remove a classe 'caixa-vermelha' da div
+    divCaixa01.classList.remove("caixa-vermelha");
+    // adicionar a classe 'caixa-azul' na div
+    divCaixa01.classList.add("caixa-azul");
+}
+
+function ArcoIris() {
+    const cores = ["vermelha", "azul", "laranja", "amarela", "verde", "anil", "violeta"];
+    const divCaixa01 = document.getElementById("caixa-01");
+    debugger
+    for (let i = 0; i < cores.length; i++) {
+        // alert(cores[i]);
+        setTimeout(() => {
+            const cor = `caixa-${cores[i]}`;
+            divCaixa01.classList = [cor];
+        }, i * 1000);
+        
+    }
+
+}
+
+function apresentarCamposPessoaFisica(){
+    const divCamposPf = document.getElementById("campos-pf");
+    divCamposPf.classList.remove("hidden");
+
+    const divCamposPj = document.getElementById("campos-pj");
+    divCamposPj.classList.remove("hidden");
+}
+
+function apresentarCamposPessoaJuridica(){
+    const divCamposPj = document.getElementById("campos-pj");
+    divCamposPj.classList.remove("hidden");
+
+    const divCamposPf = document.getElementById("campos-pf");
+    divCamposPf.classList.add("hidden");
+}
